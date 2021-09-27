7a570fd6e675a01ba9d9c19b5d1ceee74094d13b
Mauricio <shinji.256@gmail.com>
2021-09-27 14:12:56 -0500

    UI changes, filter options in select inputs, show quote amounts

--- main.js
+++ main.js
@@ -2,13 +2,14 @@
 Moralis.initialize("T6WKh57TyasHZOBP9AP9MMW9ATVzeWDvLRUozCq4");
 Moralis.serverURL = "https://hfmv8828kftz.bigmoralis.com:2053/server";
 
-
 let dex;
 let userSelectedChain;
 let userAddress;
 let tokenOptionsList = [];
+let amountChosenToSwap;
 let swapFromTokenAddress;
 let swapToTokenAddress;
+let swapQuote;
 
 //LOGIN FUNCTION WITH MORALIS
 async function login() {
@@ -21,9 +22,9 @@ async function login() {
         }
         //USER LOGGED IN
         userAddress = user.get('ethAddress');
-        const chainIdDec = await web3.eth.getChainId(); // 56 BSC - 1 ETH - 137 MATIC - 4 RinkeByTest 
+        const chainIdDec = await web3.eth.getChainId(); // 56 BSC - 1 ETH - 137 MATIC - 4 RinkebyTest 
         userSelectedChain = chainIdDec == 56 ? 'bsc' : chainIdDec == 1 ? 'eth' : chainIdDec == 137 ? 'polygon' : 'eth';
-        console.log('Connected to: '+userSelectedChain+' ('+chainIdDec+')');
+        //console.log('Connected to: '+userSelectedChain+' ('+chainIdDec+')');
         getAvailableTokens(userSelectedChain);
         //HIDE
         document.querySelector(".btn-login-container").style.display = "none";
@@ -98,6 +99,63 @@ function buildDropDowns(tokenOptionsList){
 }
 */
 
+(function () {
+    // the IIFE lets us use a local variable store information
+    var optionsCache = [];
+
+    // add option values to the cache
+    function optionsArray(select) {
+        var reduce = Array.prototype.reduce;
+        return reduce.call(select.options, function addToCache(options, option) {
+            options.push(option);
+            return options;
+        }, []);
+    }
+    // give a list of options matching the filter value
+    function filterOptions(filterValue, optionsCache) {
+        return optionsCache.reduce(function filterCache(options, option) {
+            var optionText = option.textContent;
+            if (option.text.toLowerCase().match(filterValue.toLowerCase())) {
+                options.push(option);
+            }
+            return options;
+        }, []);
+    }
+    // replace the current options with the new options
+    function replaceOptions(select, options) {
+        while (select.options.length > 0) {
+            select.remove(0);
+        }
+        options.forEach(function addOption(option) {
+            select.add(option);
+        });
+    }
+    // cache the options (if need be), and filter the options
+    function filterFromOptionsHandler(evt) {
+        var filterField = evt.target;
+        var targetSelect = document.getElementById("swap-from");
+        if (optionsCache.length < 1) {
+            optionsCache = optionsArray(targetSelect);
+        }
+        var options = filterOptions(filterField.value, optionsCache);
+        replaceOptions(targetSelect, options);
+    }
+    function filterToOptionsHandler(evt) {
+        var filterField = evt.target;
+        var targetSelect = document.getElementById("swap-to");
+        if (optionsCache.length < 1) {
+            optionsCache = optionsArray(targetSelect);
+        }
+        var options = filterOptions(filterField.value, optionsCache);
+        replaceOptions(targetSelect, options);
+    }
+    // attach filter event to trigger on keyup
+    var filterFrom = document.getElementById("filterFrom");
+    filterFrom.addEventListener("keyup", filterFromOptionsHandler, false);
+    var filterTo = document.getElementById("filterTo");
+    filterTo.addEventListener("keyup", filterToOptionsHandler, false);
+}());
+
 async function selectedSwapFromToken() {
     document.querySelector("#swap-from-image-box").innerHTML = '';
     swapFromTokenAddress = document.querySelector("#swap-from").value;
@@ -106,8 +164,9 @@ async function selectedSwapFromToken() {
     image.alt = document.querySelector("#swap-from").selectedOptions[0].getAttribute('name');
     image.classList = 'selected-token-image'
     document.querySelector("#swap-from-image-box").appendChild(image);
-
-    console.log(swapFromTokenAddress)
+    getBalance(swapFromTokenAddress);
+    //console.log(swapFromTokenAddress)
+    getQuote()
 }
 async function selectedSwapToToken() {
     document.querySelector("#swap-to-image-box").innerHTML = '';
@@ -117,33 +176,80 @@ async function selectedSwapToToken() {
     image.alt = document.querySelector("#swap-to").selectedOptions[0].getAttribute('name');
     image.classList = 'selected-token-image'
     document.querySelector("#swap-to-image-box").appendChild(image);
-
-    console.log(swapToTokenAddress)
+    //console.log(swapToTokenAddress)
+    getQuote()
 }
 
 document.querySelector("#swap-from-amount").addEventListener("change", ()=>{
     if(swapFromTokenAddress !== '' && swapToTokenAddress !== '' && document.querySelector("#swap-from-amount").value > 0){
+        document.querySelector("#swap-to-amount").value = "Loading..."
         getQuote()
     }
 });
 
+async function getBalance(token) {
+    console.log(token);
+    const options = {
+        owner_address: userAddress,
+        spender_address: userAddress,
+        address: token
+    };
+    const allowance = await Moralis.Web3API.token.getTokenAllowance(options);
+    console.log(allowance);
+}
+
 async function getQuote() {
-    let amountChosen = document.querySelector("#swap-from-amount").value;
-    const quote = await Moralis.Plugins.oneInch.quote({
+    if(swapFromTokenAddress !== '' && swapToTokenAddress !== '' && document.querySelector("#swap-from-amount").value > 0){
+        amountChosenToSwap = document.querySelector("#swap-from-amount").value;
+        const quote = await Moralis.Plugins.oneInch.quote({
+            chain: userSelectedChain, // The blockchain you want to use (eth/bsc/polygon)
+            fromTokenAddress: swapFromTokenAddress, // The token you want to swap
+            toTokenAddress: swapToTokenAddress, // The token you want to receive
+            amount: Number.parseFloat(amountChosenToSwap),
+        });
+        swapQuote = quote;
+        if(swapQuote.error){
+            document.querySelector("#swap-error").innerHTML = "Error: "+swapQuote.message+'('+swapQuote.error+')';
+        }else{
+            hasAllowance();
+            document.querySelector("#swap-error").innerHTML = '';
+            document.querySelector("#swap-to-amount").value = swapQuote.toTokenAmount;
+            document.querySelector("#estimated-gas").innerHTML = 'Estimated Gas Fee: '+swapQuote.estimatedGas;
+            document.querySelector("#start-swap").disabled = false;
+            //console.log(swapQuote.protocols);
+        }
+    }
+  }
+
+  async function hasAllowance() {
+    const allowance = await Moralis.Plugins.oneInch.hasAllowance({
+      chain: userSelectedChain, // The blockchain you want to use (eth/bsc/polygon)
+      fromTokenAddress: swapFromTokenAddress, // The token you want to swap
+      fromAddress: swapToTokenAddress, // Your wallet address
+      amount: Number.parseFloat(amountChosenToSwap),
+    });
+    console.log(`The user has enough allowance: ${allowance}`);
+  }
+
+  async function approve() {
+    await Moralis.Plugins.oneInch.approve({
+      chain: userSelectedChain, // The blockchain you want to use (eth/bsc/polygon)
+      tokenAddress: swapFromTokenAddress, // The token you want to swap
+      fromAddress: userAddress, // Your wallet address
+    });
+    swap();
+  }
+
+  async function swap() {
+    const receipt = await Moralis.Plugins.oneInch.swap({
       chain: userSelectedChain, // The blockchain you want to use (eth/bsc/polygon)
       fromTokenAddress: swapFromTokenAddress, // The token you want to swap
       toTokenAddress: swapToTokenAddress, // The token you want to receive
-      amount: 1,
+      amount: Number.parseFloat(amountChosenToSwap),
+      fromAddress: userAddress, // Your wallet address
+      slippage: 1,
     });
-    if(quote.error){
-        console.log("Quote: "+quote.message+'('+quote.error+')');
-    }else{
-        console.log("Quote: "+quote.toTokenAmount+'(Received Amount)');
-        console.log("Quote: "+quote.estimatedGas+'(Estimated Gas Fee)');
-    }
-    Object.keys(quote).forEach(value => {
-        console.log(value);
-    })
+    console.log(receipt);
   }
 
 /*LISTENERS*/
@@ -151,3 +257,4 @@ document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
 document.querySelector("#swap-from").addEventListener("change", selectedSwapFromToken);
 document.querySelector("#swap-to").addEventListener("change", selectedSwapToToken);
+document.querySelector("#start-swap").addEventListener("click", approve);
